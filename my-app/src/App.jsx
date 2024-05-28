c023267fb091d717969c0e4568743d7783cc26f0
Amar <108946547+AmarVcode@users.noreply.github.com>
2024-05-29 02:29:02 +0530

    upadted

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.scss'
 import  Header  from "./component/header/header.jsx";
+import  Home  from "./component/home/home.jsx";
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Home/>
     </>
   )
 }
---
+++ my-app/src/component/home/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./home.scss"
+
+const home = () => {
+  return (
+    <div>
+
+<h1 className="main-heading">
+Epic <span className="text-gradient">Builds</span> for Epic <span className="bg-gradient">Wins</span>
+</h1>
+
+    </div>
+  )
+}
+
+export default home
